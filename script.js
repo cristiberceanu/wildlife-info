069f8fb216b14bc6fd901d4a137b0a21da7b384f
cristiberceanu <cristianberceanu@gmail.com>
2023-03-15 21:48:25 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+async function fetchData() {
+  const taxonId = document.getElementById('taxonId').value;
+  const apiUrl = `http://eol.org/api/pages/1.0.json?id=${taxonId}&images=2&texts=2&videos=0&details=true&format=json`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    document.getElementById('speciesName').textContent = data.scientificName;
+    document.getElementById('speciesImage').src = data.dataObjects[0].eolMediaURL;
+    document.getElementById('speciesDescription').textContent = data.dataObjects[1].description;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
